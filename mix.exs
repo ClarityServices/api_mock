8859b7acb225dd7b0cb98638b2284ff232bf2798
Chris Lamb <lamb.chrisr@gmail.com>
2018-11-20 07:53:12 -0500

    Update dependencies for 1.4

--- mix.exs
+++ mix.exs
@@ -33,12 +33,15 @@ defmodule ApiMock.Mixfile do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.3.3"},
+      {:phoenix, "~> 1.4.0"},
       {:phoenix_pubsub, "~> 1.0"},
+      {:ecto_sql, "~> 3.0"},
       {:phoenix_ecto, "~> 3.2"},
       {:postgrex, ">= 0.0.0"},
+      {:jason, "~> 1.0"},
       {:gettext, "~> 0.11"},
-      {:cowboy, "~> 1.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:plug, "~> 1.7"}
     ]
   end
 
